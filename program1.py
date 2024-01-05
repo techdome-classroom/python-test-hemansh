409c4ba8a435eadcc57d661f506ecc7fcd79bc7a
hemansh <hemanshagnihotri27@gmail.com>
2024-01-04 23:50:57 -0800

    solution to the problems

--- program1.py
+++ program1.py
@@ -1,12 +1,20 @@
 class Solution(object):
     def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        pass
+        stack = []
+        for i in s:
+            if i == '(':
+                stack.append(')')
+            elif i == '{':
+                stack.append('}')
+            elif i == '[':
+                stack.append(']')
+            elif not stack or stack.pop() != i:
+                return False
+        return len(stack) == 0
     
 
+# obj = Solution()
+# print(obj.isValid('()'))
 
 
   
--- program2.py
+++ program2.py
@@ -1,7 +1,12 @@
 class Solution(object):
     def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        pass
+        map = {'I': 1,'V': 5,'X': 10,'L': 50,'C': 100,'D': 500,'M': 1000}
+        n = len(s)
+        ans = 0
+        for i in range(n):
+            if i < n - 1 and map[s[i]] < map[s[i+1]]:
+                ans -= map[s[i]]
+            else:
+                ans += map[s[i]]
+        
+        return ans
